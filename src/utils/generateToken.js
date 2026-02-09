a619364b8d415e1ef06911cc6345429638d98339
Vaibhav-gits <vaibhavvdy@gmail.com>
2026-02-09 11:59:20 +0530

    Delete src directory

--- src/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '7d', // 7 days
-  });
-};
-
-module.exports = generateToken;
